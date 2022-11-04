5b2791d5f83929c66f0a6504134db58d23e6601b
Arlan Nurkhozhin <dearmordor@gmail.com>
2022-11-04 16:11:27 +0100

    Create exceptions

---
+++ src/main/java/cz/cvut/kbss/ear/mroom/exception/BaseException.java
@@ -0,0 +1,20 @@
+package cz.cvut.kbss.ear.mroom.exception;
+
+public class BaseException extends RuntimeException {
+
+    public BaseException() {
+    }
+
+    public BaseException(String message) {
+        super(message);
+    }
+
+    public BaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BaseException(Throwable cause) {
+        super(cause);
+    }
+
+}
---
+++ src/main/java/cz/cvut/kbss/ear/mroom/exception/NotEnoughMoney.java
@@ -0,0 +1,7 @@
+package cz.cvut.kbss.ear.mroom.exception;
+
+public class NotEnoughMoney extends BaseException{
+    public NotEnoughMoney(String message) {
+        super(message);
+    }
+}
