d3c8f55421934d16cbd2f18d277afb227e80265e
Arlan Nurkhozhin <dearmordor@gmail.com>
2022-11-10 21:47:29 +0100

    change hibernate version to the same verson + refactor

--- src/main/java/cz/cvut/kbss/ear/mroom/MeetingRoomReservationApplication.java
+++ src/main/java/cz/cvut/kbss/ear/mroom/MeetingRoomReservationApplication.java
@@ -2,11 +2,21 @@ package cz.cvut.kbss.ear.mroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication()
+@RestController
 public class MeetingRoomReservationApplication {
     public static void main(String[] args) {
         SpringApplication.run(MeetingRoomReservationApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public List<String> hello() {
+        return List.of("Hello", "World");
     }
 
 }
