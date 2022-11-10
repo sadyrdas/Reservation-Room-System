d56a906c74b0ea2c48e695b388bd0757eb90392b
Dastan Sadylirayev <sadyrdas@fel.cvut.cz>
2022-11-11 01:56:10 +0600

    Some repairs

--- data.sql
+++ data.sql
@@ -1,8 +1,54 @@
-CREATE TABLE "user" (
-    id integer NOT NULL,
-    email character varying(255) not null,
-    firstname character varying(255) not null,
-    lastname character varying(255) not null,
-    password character varying(255) not null,
-    money double precision
-)
+CREATE TABLE User
+(
+    id        integer      NOT NULL PRIMARY KEY,
+    email     varchar(255) not null,
+    firstname varchar(255) not null,
+    lastname  varchar(255) not null,
+    password  varchar(255) not null,
+    money     double       not null
+);
+CREATE TABLE Admin
+(
+    id        integer      NOT NULL PRIMARY KEY,
+    email     varchar(255) not null,
+    firstname varchar(255) not null,
+    lastname  varchar(255) not null,
+    password  varchar(255) not null
+);
+CREATE TABLE StudyRoom
+(
+    id          integer      NOT NULL PRIMARY KEY,
+    capacity    integer      not null,
+    price       double       not null,
+    reservation varchar(255) not null
+);
+
+CREATE TABLE Slot
+(
+    id        integer      NOT NULL PRIMARY KEY,
+    available BIT          NOT NULL,
+    day       varchar(255) NOT NULL,
+    CONSTRAINT slot_fk_day FOREIGN KEY (day) references Day (name)
+);
+CREATE TABLE Day
+(
+    id   integer      NOT NULL PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    week varchar (255) NOT NULL,
+    CONSTRAINT day_fk_week FOREIGN KEY (week) references Week (numberOfWeek)
+);
+
+CREATE TABLE Week
+(
+    id integer NOT NULL PRIMARY KEY ,
+    numberOfWeek integer NOT NULL
+
+
+);
+
+CREATE TABLE Payment
+(
+    id            integer      NOT NULL PRIMARY KEY,
+    paymentMethod varchar(255) NOT NULL,
+    status        BIT          NOT NULL
+);
