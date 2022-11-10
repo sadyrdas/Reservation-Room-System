1f548ad864b3f606387895948fe8149a9b5e1ad2
Arlan Nurkhozhin <dearmordor@gmail.com>
2022-11-10 22:47:44 +0100

    finally, Spring works!

--- src/main/java/cz/cvut/kbss/ear/mroom/MeetingRoomReservationApplication.java
+++ src/main/java/cz/cvut/kbss/ear/mroom/MeetingRoomReservationApplication.java
@@ -2,19 +2,20 @@ package cz.cvut.kbss.ear.mroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@SpringBootApplication()
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 @RestController
 public class MeetingRoomReservationApplication {
     public static void main(String[] args) {
         SpringApplication.run(MeetingRoomReservationApplication.class, args);
     }
 
-    @GetMapping("/hello")
+    @GetMapping
     public List<String> hello() {
         return List.of("Hello", "World");
     }
