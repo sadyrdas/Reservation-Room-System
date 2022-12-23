a7eab8131f56ea15e2ef70a77e6f6ed332c99a92
Arlan Nurkhozhin <dearmordor@gmail.com>
2022-12-23 19:06:44 +0100

    Add exception handlers

---
+++ src/main/java/cz/cvut/kbss/ear/mroom/exception/CourseAlreadyExists.java
@@ -0,0 +1,7 @@
+package cz.cvut.kbss.ear.mroom.exception;
+
+public class CourseAlreadyExists extends BaseException {
+    public CourseAlreadyExists(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/cz/cvut/kbss/ear/mroom/exception/UserAlreadyExists.java
@@ -0,0 +1,7 @@
+package cz.cvut.kbss.ear.mroom.exception;
+
+public class UserAlreadyExists extends BaseException {
+    public UserAlreadyExists(String message) {
+        super(message);
+    }
+}
